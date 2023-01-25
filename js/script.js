07bf746703a1d29f06c69e46f7e481e5fddedcd8
CilieTr <ctroim1991@gmail.com>
2023-01-25 13:36:29 +0100

    endret btn const

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const productInput = document.querySelector("#productInput");
-const productSubmit = document.querySelector("#productSubmit");
+const productSubmitBtn = document.querySelector("#productSubmit");
 const productList = document.querySelector("#productList");
 
 const products = ["melk", "skinke", "ost", "kaffe"];
+
