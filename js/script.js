9f4fc8d60be6ed907943fa3e7338f7d2434b5428
CilieTr <ctroim1991@gmail.com>
2023-01-25 14:00:33 +0100

    lagt inn funksjon for og fjerne produkt og å legge til produkt i liste

--- js/script.js
+++ js/script.js
@@ -7,13 +7,24 @@ const products = ["melk", "skinke", "ost", "kaffe"];
 productSubmitBtn.addEventListener("click", addProduct);
 
 function addProduct() {
-    products.push(productInput.value);
-    showProduct();
+  products.push(productInput.value);
+  showProduct();
+}
+
+function removeProduct(i) {
+  products.splice(i, 1);
+  showProduct();
 }
 
 function showProduct() {
-    productList.innerHTML ="";
-    productInput.value = "";
+  productList.innerHTML = "";
+  productInput.value = "";
+}
+
+for (let i = 0; i < products.length; i++) {
+  productList.innerHTML += `<li onclick ="removeProduct(${[i]})">${
+    products[i]
+  } </li>`;
 }
 
 showProduct();
