ffb3cb40a02cbf4e21e5fec025da6d1d5c4b7863
CilieTr <ctroim1991@gmail.com>
2023-01-25 13:33:18 +0100

    lagt inn liste

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 const productInput = document.querySelector("#productInput");
 const productSubmit = document.querySelector("#productSubmit");
 const productList = document.querySelector("#productList");
+
+const products = ["melk", "skinke", "ost", "kaffe"];
