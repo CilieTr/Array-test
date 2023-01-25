71c7e42212c4aaad631e2456008aa26accf9a180
CilieTr <ctroim1991@gmail.com>
2023-01-25 13:29:38 +0100

    jeg har lagt til variabler

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const productInput = document.querySelector("#productInput");
+const productSubmit = document.querySelector("#productSubmit");
+const productList = document.querySelector("#productList");
